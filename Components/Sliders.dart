d761af2134f0eb2941f27869488cc1b5d3cf8265
Muhammad Zeeshan Latif <110631680+MuhammadZeshanLatif@users.noreply.github.com>
2023-09-08 00:20:52 +0500

    yes

--- Components/Sliders.dart
+++ Components/Sliders.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import './colors.dart';
 import './Buttons.dart';
-import 'package:flutter/material.dart';
-
 class Hadings {
   final String? line;
   final double? paddindTop;
--- main.dart
+++ main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import './Components/Sliders.dart';
 import './Components/colors.dart';
-import './Components/RecommendedPageLayer1.dart';
-import 'Components/UpperNavSlider.dart';
-
 void main() {
   runApp(MyApp());
 }
@@ -22,21 +19,6 @@ class MyApp extends StatelessWidget{
 }
 class HomePage extends StatelessWidget {
   PageController pageController = PageController(initialPage: 0);
-
-  List<Widget> pages = [
-    Container(
-      color: Colors.red,
-      child: Center(child: Text("Page 1")),
-    ),
-    Container(
-      color: Colors.green,
-      child: Center(child: Text("Page 2")),
-    ),
-    Container(
-      color: Colors.blue,
-      child: Center(child: Text("Page 3")),
-    ),
-  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,7 +29,6 @@ class HomePage extends StatelessWidget {
       //body:Uppernavslider(),
       body: SliderPageView(), 
       // body: SliderPageView(),
-
       // body: Recommendedpage(),
     );
   }
